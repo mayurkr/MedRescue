6e3040dd714a9db6da9f3c51891f65bd3d5539ce
Prasad Honavar <prasadhonavar@gmail.com>
2015-09-15 14:21:22 +0530

    changed url to /

--- rescue/urls.py
+++ rescue/urls.py
@@ -4,5 +4,5 @@ urlpatterns = [
     #    url(r'^$', views.post_list, name='post_list'),
     #    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     #    url(r'^post/new/$', views.post_new, name='post_new'),
-    url(r'^SOS', views.sos, name='sos')
+    url(r'^$', views.sos, name='sos')
 ]
